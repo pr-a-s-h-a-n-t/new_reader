14ff3e19a209715ddd7654616b5f13032fd2060f
Prasahnt <prashant.mishra@leadtorev.com>
2023-07-26 08:03:41 +0530

    stp

---
+++ my-app/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import Test from '@/conponents/Test'
+
+export default function Home() {
+  return (
+    <>
+    <Test />
+    </>
+  )
+}
